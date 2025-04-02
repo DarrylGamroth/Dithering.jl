a04a0f46910f618e0e7f539812f585284f71de1d
Darryl Gamroth <dgamroth@rubus.ca>
2025-04-02 10:47:15 -0700

    Change update to step

--- src/Dithering.jl
+++ src/Dithering.jl
@@ -1,5 +1,7 @@
 module Dithering
 
+export Dither, target!, target, current, output, lo!, lo, hi!, hi, step_size!, step_size, reset!, step!
+
 mutable struct Dither{T<:Number,A<:AbstractVector{T}}
     target::A
     current::A
@@ -10,7 +12,7 @@ mutable struct Dither{T<:Number,A<:AbstractVector{T}}
     const invlsb::T
     lo::T
     hi::T
-    step::T
+    step_size::T
 
     function Dither{T,A}(target, current, output, error, num_actuators, lsb, lo, hi, step) where {T<:Number,A<:AbstractVector{T}}
         if length(target) == length(current) == length(output) == length(error) != num_actuators
@@ -20,15 +22,7 @@ mutable struct Dither{T<:Number,A<:AbstractVector{T}}
     end
 end
 
-function Dither(::Type{T}, num_actuators, bits::UInt, lo, hi, step) where {T<:Number}
-    return Dither{T,Vector{T}}(zeros(T, num_actuators),
-        zeros(T, num_actuators),
-        zeros(T, num_actuators),
-        zeros(T, num_actuators),
-        num_actuators, 2 / (2^bits - 1), lo, hi, step)
-end
-
-function Dither(num_actuators, bits::UInt, lo::T, hi::T, step::T) where {T<:Number}
+function Dither(::Type{T}, num_actuators, bits::Int, lo, hi, step) where {T<:Number}
     return Dither{T,Vector{T}}(zeros(T, num_actuators),
         zeros(T, num_actuators),
         zeros(T, num_actuators),
@@ -44,8 +38,8 @@ lo!(d::Dither, value) = d.lo = value
 lo(d::Dither) = d.lo
 hi!(d::Dither, value) = d.hi = value
 hi(d::Dither) = d.hi
-step!(d::Dither, value) = d.step = value
-step(d::Dither) = d.step
+step_size!(d::Dither, value) = d.step_size = value
+step_size(d::Dither) = d.step_size
 
 function reset!(d::Dither{T,A}) where {T<:Number,A<:AbstractVector{T}}
     fill!(d.target, zero(T))
@@ -54,12 +48,12 @@ function reset!(d::Dither{T,A}) where {T<:Number,A<:AbstractVector{T}}
     fill!(d.error, zero(T))
 end
 
-function update!(d::Dither{T,A}) where {T<:Number,A<:AbstractVector{T}}
+function step!(d::Dither{T,A}) where {T<:Number,A<:AbstractVector{T}}
     lo = d.lo
     hi = d.hi
     lsb = d.lsb
     invlsb = d.invlsb
-    step_max = d.step
+    step_max = d.step_size
 
     @inbounds for i in eachindex(d.target, d.current, d.error, d.output)
         # Load state
@@ -79,7 +73,7 @@ function update!(d::Dither{T,A}) where {T<:Number,A<:AbstractVector{T}}
         y = u - e + dither
 
         # Quantize y
-        v = floor(y * invlsb) * lsb
+        v = round(y * invlsb) * lsb
 
         # Calculate quantization error
         e = v - y
